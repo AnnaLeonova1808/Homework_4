25b6fcb67b916562e453a18d9d24e837438af223
AnnaLeonova1808 <122633555+AnnaLeonova1808@users.noreply.github.com>
2023-02-24 17:57:41 +0300

    Add files via upload

---
+++ Homework_4/script/script.js
@@ -0,0 +1,23 @@
+const btn_1 = document.querySelector('.btn_1');
+const btn_2 = document.querySelector('.btn_2');
+const btn_3 = document.querySelector('.btn_3');
+const btn_4 = document.querySelector('.btn_4');
+const btn_5 = document.querySelector('.btn_5');
+
+
+btn_1.addEventListener('click',function(){
+    alert('#16a085');
+    
+})
+btn_2.addEventListener('click',function(){    
+    alert('#9b59b6');
+})
+btn_3.addEventListener('click',function(){
+    alert('#e67e22');
+})
+btn_4.addEventListener('click',function(){
+    alert('#3498db');
+})
+btn_5.addEventListener('click',function(){
+    alert('#e74c3c');
+})
